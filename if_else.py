dfeabe7a9f4ded6c5133b0485b7951c9e02374d1
anilshetty20 <anilshetty6615@gmail.com>
2025-09-18 23:04:28 +0530

    Drive or not based on age using if else

---
+++ if_else.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your age: "))
+print("your age is ",a)
+print("Let me check you can drive or not based on your age")
+if a>=18:
+    print("You can drive as your age is above 18")
+    print("Hurray! I can go for a long ride")
+else:
+    print("You cannot drive as your age is not above 18")
