5ad083bb83fbb1742beaf6a8fb6d853385926a7c
anilshetty20 <anilshetty6615@gmail.com>
2025-09-19 12:40:07 +0530

    Updated the code for the greeting acc to time

--- Good_morning.py
+++ Good_morning.py
@@ -1,12 +1,13 @@
 import time
 
-curr_time=int(time.strftime("%H"))
-
-if 5<=curr_time<=11:
+curr_time= time.strftime('%H:%M:%S')
+print("The current time in India is :",curr_time)
+curr_hour=int(time.strftime('%H'))
+if 5<=curr_hour<=11:
     print("Good Morning")
-elif 12<=curr_time<=4:
+elif 12<=curr_hour<=16:
     print("Good Afternoon")
-elif 5<=curr_time<=7:
+elif 17<=curr_hour<=20:
     print("Good Evening")
 else:
     print("Good Night")
