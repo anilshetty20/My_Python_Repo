c6ac06a2d429186445c5ec365ae4283bdda8d7e6
anilshetty20 <anilshetty6615@gmail.com>
2025-09-23 16:10:21 +0530

    simple list in python

---
+++ lists.py
@@ -0,0 +1,6 @@
+list=[1,2,3,4,5,"anil","shetty",7,8,9,10]
+print(type(list))
+print(len(list)) #lenghtu of the string
+print(list[0]) #accesscing the the lists
+print(list[:])   #printing all the elements in the list
+print(list[1:8:2]) # takes 1 to 8 with 2 step
