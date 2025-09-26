737f518f87865bdbe9c4e0bb5b5ea669f220466b
anilshetty20 <anilshetty6615@gmail.com>
2025-09-26 17:08:36 +0530

    Docstring in python

---
+++ docstrings.py
@@ -0,0 +1,6 @@
+#This is used to usnderstand the funations properly
+def square(n): 
+    '''This function is used print the square of the number n'''  #This is docstirng and this witten just below the function if its not below the function then it is not a doc string
+    print(n**2)
+square(5)
+print(square.__doc__)   #this is used to acces the docstring
