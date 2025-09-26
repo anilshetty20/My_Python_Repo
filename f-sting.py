5f171bfc6ba6448fc94917038dc5c7d0648a9fd2
anilshetty20 <anilshetty6615@gmail.com>
2025-09-26 16:55:54 +0530

    f-strings in python

---
+++ f-sting.py
@@ -0,0 +1,6 @@
+#f-string is strring formating
+letter="Hello my name is {} and I am from {}"
+name="Anil"
+country="India"
+print(letter.format(name,country))
+print(f"Hello my name is {name} and I am from {country}") #this is anothe r way of using the f-string
