dff260e3569c990ffdf697b9a8540422208624a6
anilshetty20 <anilshetty6615@gmail.com>
2025-09-29 10:57:39 +0530

    sets in pyhton

---
+++ sets.py
@@ -0,0 +1,12 @@
+# Sets are unorderd collection of data items so items cannot be indexed
+# Set items are seperated by ,
+# Sets are unclosed within curly braces
+# Sets dosent print the repeated data items
+a={1,2,4,2,4,5,6}
+print(a)
+b={}    #this is not the empty set it is empty dictonary
+print(type(a))
+print(type(b))
+
+c=set()  # this assigns the empty sets
+print(type(c))
