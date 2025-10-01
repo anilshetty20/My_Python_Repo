b64939aca6b3393a52a4c8b8b9108c2f99d64ca7
anilshetty20 <anilshetty6615@gmail.com>
2025-10-01 15:44:16 +0530

    Finally in python

--- finally_in_python.py
+++ finally_in_python.py
@@ -1,1 +1,11 @@
 # Finally is a keyword which is always execuated irresppective of the try and except
+try:
+    l=[1,2,3,4,5,6]
+    i=int(input("Enter the index number :"))
+    print(l[i])
+except:
+    print("some error oocured !")
+
+finally:
+    print("This will print irrecpective of the try and except")
+    
