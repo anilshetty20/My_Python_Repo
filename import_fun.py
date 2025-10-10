1805a3edeb5aea50ba683984511b9e4f36320fc0
anilshetty20 <anilshetty6615@gmail.com>
2025-10-10 12:13:09 +0530

    import in python

---
+++ import_fun.py
@@ -0,0 +1,4 @@
+#import math            #this is the import function
+import math as s        # this is another way of declaring the import
+print(s.floor(20.3039))
+print(s.sqrt(9))  #prints 9 is which numbers square
