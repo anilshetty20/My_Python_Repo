306461503df12d3dc8e998616973a0e12eab29ca
anilshetty20 <anilshetty6615@gmail.com>
2025-10-10 12:31:41 +0530

    using import from another file

---
+++ anil.py
@@ -0,0 +1,4 @@
+# this is file which will be used as a import in main.py file
+def welcome():
+    print("Hello anil welcome to Python learning")
+anil= "Anil is smart"
---
+++ main.py
@@ -0,0 +1,4 @@
+from anil import welcome,anil  #here this will import welcome, harry from anil.py file
+from anil import *          #here this is optional this will import everything from anil.py file using *
+welcome()                   #prints what is inside welcome function which is in anil.py
+print(anil)                 #prints the variable anil which is in anil.py
