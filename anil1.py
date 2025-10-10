7b667924abf640a13d743633c85eb60f87401e07
anilshetty20 <anilshetty6615@gmail.com>
2025-10-10 14:39:19 +0530

    Error code

---
+++ anil1.py
@@ -0,0 +1,5 @@
+def welcome():
+    print("Hello how are you")
+
+if __name__=="__main1__":
+    welcome()
---
+++ main1.py
@@ -0,0 +1,3 @@
+import anil1   # This alone will improt from anil1.py and print the welcome function
+anil1.welcome()  #this also print so it will become two times
+
